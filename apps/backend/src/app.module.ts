1ecb46719274b0f8f7e83cbd451fcfbdef9a8194
ttkhiem <ttkhiem310503@gmail.com>
2025-11-08 17:43:18 +0700

    hotfix(backend): remove codebase hono >< nestjs

--- apps/backend/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserManagementModule } from '../features/user-management/user-management.module';
-
-@Module({
-  imports: [UserManagementModule],
-})
-export class AppModule {}
